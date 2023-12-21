6082af50f7207a59dc0f6c142e3cddbc8cc664a8
x1210x <x1210x@gmail.com>
2023-12-21 18:04:54 +0900

    build fix

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fokedex/util/timber.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 
